3aea3906d54ee762d20ef09fb90ec8e6e082be82
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2023-08-28 15:25:02 +0300

    feat(ProductsController): add CRUD operations

--- ERPSystem.API/Controllers/ProductsController.cs
+++ ERPSystem.API/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
+using ERPSystem.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace ERPSystem.API.Controllers
 {
@@ -6,6 +8,7 @@ namespace ERPSystem.API.Controllers
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
+        static private readonly List<Product> products = new List<Product>();
 
         private readonly ILogger<ProductsController> _logger;
 
@@ -13,5 +16,59 @@ namespace ERPSystem.API.Controllers
         {
             _logger = logger;
         }
+
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            return new JsonResult(products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetProduct(int id)
+        {
+            Product product = products.Find(product => product.id == id);
+            if (product == null) return new NotFoundResult();
+            return new JsonResult(product);
+        }
+
+        [HttpPost]
+        public ActionResult PostProduct([FromQuery] string name, [FromQuery] int price)
+        {
+
+            Product product = new Product { id = products.Count(), name = name, price = price };
+
+            if (product == null) return new NotFoundResult();
+
+            products.Add(product);
+
+            return new JsonResult(product);
+
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateProduct(int id, [FromQuery] string? name, [FromQuery] int? price)
+        {
+
+            Product product = products.Find(product => product.id == id);
+
+            if (product == null) return new NotFoundResult();
+
+            if (name != null) product.name = name;
+
+            if (price != null) product.price = (int)price;
+
+            return new JsonResult(product);
+
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteProduct(int id)
+        {
+            Product product = products.Find(product => product.id == id);
+            if (product == null) return new NotFoundResult();
+            products.RemoveAt(id);
+            return new JsonResult(product);
+        }
+
     }
 }
--- ERPSystem.API/Models/Product.cs
+++ ERPSystem.API/Models/Product.cs
@@ -2,5 +2,8 @@
 {
     public class Product
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
     }
 }
